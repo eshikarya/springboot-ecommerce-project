db8e44fb48679bf83a55da215566f8959c8b9824
Arya <902439@cognizant.com>
2025-04-16 12:19:44 +0530

    Pagination-Sorting-API Response 1.1

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.controller;
 
+import com.ecommerce.project.config.AppConstants;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
@@ -25,8 +26,13 @@ public class CategoryController {
 
 //    @GetMapping("/public/categories")
     @RequestMapping(value = "/public/categories",method = RequestMethod.GET)
-    public ResponseEntity<CategoryResponse> getAllCategories(){
-        CategoryResponse categoryResponse =  categoryService.getAllCategories();
+    public ResponseEntity<CategoryResponse> getAllCategories(
+            @RequestParam(name="pageNumber",required = false,defaultValue = AppConstants.PAGE_NUMBER)Integer pageNumber,
+            @RequestParam(name="pageSize",required = false, defaultValue = AppConstants.PAGE_SIZE)Integer pageSize,
+            @RequestParam(name="sortBy", defaultValue = AppConstants.SORT_CATEGORIES_BY) String sortBy,
+            @RequestParam(name="sortOrder", defaultValue = AppConstants.SORT_DIR) String sortOrder
+    ){
+        CategoryResponse categoryResponse =  categoryService.getAllCategories(pageNumber,pageSize,sortBy,sortOrder);
         return new ResponseEntity<>(categoryResponse,HttpStatus.OK);
     }
 
--- src/main/java/com/ecommerce/project/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/project/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.exceptions;
 
+import com.ecommerce.project.payload.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -25,15 +26,17 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFound(ResourceNotFoundException e){
+    public ResponseEntity<APIResponse> handleResourceNotFound(ResourceNotFoundException e){
         String message = e.getMessage();
-        return new ResponseEntity<>(message,HttpStatus.NOT_FOUND);
+        APIResponse response = new APIResponse(message,false);
+        return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(APIException.class)
-    public ResponseEntity<String> handleApiException(APIException e){
+    public ResponseEntity<APIResponse> handleApiException(APIException e){
         String message = e.getMessage();
-        return new ResponseEntity<>(message,HttpStatus.BAD_REQUEST);
+        APIResponse response = new APIResponse(message,false);
+        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
     }
 
 }
