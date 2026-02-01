75299c7dfea56d74be65d6bf9e32e09557f3d314
Eshika Arya <eshika.arya@external.telekom.de>
2026-02-02 00:08:36 +0530

    SB-ECOM 2026: First Commit

---
+++ src/main/java/com/ecommerce/project/controllers/CategoryController.java
@@ -0,0 +1,19 @@
+package com.ecommerce.project.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class CategoryController {
+
+//    @GetMapping("/public/categories")
+
+//    @PostMapping("/admin/category")
+
+//    @PutMapping("/admin/categories/{categoryId}")
+
+//    @DeleteMapping("/admin/categories/{categoryId}")
+
+
+
+}
