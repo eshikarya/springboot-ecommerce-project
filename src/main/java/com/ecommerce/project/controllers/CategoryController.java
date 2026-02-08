244eaa9dc69d75f721f25ee699023be1f05160c2
Eshika Arya <eshika.arya@external.telekom.de>
2026-02-08 19:44:12 +0530

    Section 8: Getting Started with JPA | Jakarta Persistence API and Section 9: Reduce Boilerplate Code: Introducing Lombok

--- src/main/java/com/ecommerce/project/controllers/CategoryController.java
+++ src/main/java/com/ecommerce/project/controllers/CategoryController.java
@@ -2,7 +2,6 @@ package com.ecommerce.project.controllers;
 
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,24 +1,26 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+//@Entity(name = "categories")
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
     public void setCategoryId(Long categoryId) {
         this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
     }
 }
